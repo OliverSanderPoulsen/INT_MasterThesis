9ec016c36e27a277a1afab7189b16f4e985b6ee8
OliverSanderPoulsen <oliver.sander.poulsen@gmail.com>
2023-06-27 15:36:04 +0000

    Send udp with 0 value.

--- help_files/UDP_send.py
+++ help_files/UDP_send.py
@@ -9,29 +9,10 @@ UDP_PORT = 5353
 # Create a UDP socket for internet communication
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-message = b'\x54\x68\x69\x73\x20\x69\x73\x20\x61\x20\x74\x65\x73\x74\x2e'
-
-header = 'xxx'
-ingress_port = 252
-egress_port = 113
-timestamp = 12345
-queue_occupancy = 5555
-hop_by_hop = '4-5-2-1'
-ttl = 64
-
-# Pack the fields into a binary format
-packet_data = struct.pack("!3sHHIH{}sB".format(len(hop_by_hop)),
-                          header.encode(),
-                          ingress_port,
-                          egress_port,
-                          timestamp,
-                          queue_occupancy,
-                          hop_by_hop.encode(),
-                          ttl)
-
+message = b''
 
 # Send the packet to the specified IP and port
-for x in range(10):
+for x in range(1):
     byte_val = x.to_bytes(10, 'big')
     print(byte_val)
     sock.sendto(byte_val, (UDP_IP, UDP_PORT))
